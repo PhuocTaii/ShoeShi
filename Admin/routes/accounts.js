c0de041f483015a4cf3d2981277a6e426290944a
Van Tran <thanhvan26823@gmail.com>
2023-11-10 22:56:12 +0700

    apply config specific layout

---
+++ Admin/routes/accounts.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET accounts page. */
+router.get('/accounts', function(req, res, next) {
+  res.render('accounts/index');
+});
+
+module.exports = router;
