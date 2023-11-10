54b598828b8c015d313da2f54b4cf875efe05c76
Van Tran <thanhvan26823@gmail.com>
2023-11-11 00:26:32 +0700

    modify main layout
    including header, nav-sidebar and footer

--- Admin/app.js
+++ Admin/app.js
@@ -6,8 +6,9 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/login'); 
-const dashboardRouter = require('./routes/dashboard');
+const loginRouter = require('./routes/login');
+// const dashboardRouter = require('./routes/dashboard');
+const accountRouter = require('./routes/accounts');
 
 const app = express();
 
@@ -25,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-app.use('/dashboard', dashboardRouter);
-
+// app.use('/dashboard', dashboardRouter);
+app.use('/accounts', accountRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Admin/routes/accounts.js
+++ Admin/routes/accounts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET accounts page. */
-router.get('/accounts', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('accounts/index');
 });
 
