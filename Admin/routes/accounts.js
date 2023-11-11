94bf124ae141a9f5a3ddf4dbeab0e4c9b36ea285
Van Tran <thanhvan26823@gmail.com>
2023-11-11 15:13:11 +0700

    add page accounts

--- Admin/routes/accounts.js
+++ Admin/routes/accounts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET accounts page. */
 router.get('/', function(req, res, next) {
-  res.render('accounts/index');
+  res.render('accounts/index', {extraStyles: 'accounts.css'});
 });
 
 module.exports = router;
