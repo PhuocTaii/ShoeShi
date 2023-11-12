e40925575bb75246a314df827d6c708cdc5d6ef1
binhvo292003 <95414345+binhvo292003@users.noreply.github.com>
2023-11-12 20:33:58 +0700

    half home customer

--- Customer/app.js
+++ Customer/app.js
@@ -8,6 +8,7 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const loginRouter = require("./routes/login");
 const signupRouter = require("./routes/signup");
+const homeRouter = require("./routes/home");
 
 const app = express();
 
@@ -25,7 +26,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 app.use("/signup", signupRouter);
-
+app.use("/home", homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
