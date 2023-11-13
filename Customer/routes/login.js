546617ec7cc98fd7091065cddc608660a575b15d
Van Tran <thanhvan26823@gmail.com>
2023-11-14 00:24:50 +0700

    modify login page

--- Customer/routes/login.js
+++ Customer/routes/login.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET login page. */
 router.get("/", function (req, res, next) {
-    res.render("auth/login", { layout: "layout/login" });
+    res.render("auth/login", { layout: "layout/auth", extraStyles: 'login.css' });
 });
 
 module.exports = router;
