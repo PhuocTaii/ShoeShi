86e96f0e46ecf7600746e7005d975a01da26e440
Van Tran <thanhvan26823@gmail.com>
2023-12-02 12:51:57 +0700

    add order page for admin

--- ShoeShi/routes/websiteRoutes/orderRouter.js
+++ ShoeShi/routes/websiteRoutes/orderRouter.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET accounts page. */
+/* GET orders page. */
 router.get('/orders', function (req, res, next) {
   res.render('customer/order', {
     layout: 'customer/layout/main',
+    extraStyles: 'order.css',
+  })
+})
+
+/* GET orders page for admin. */
+router.get('/admin/orders', function (req, res, next) {
+  res.render('admin/orders', {
+    layout: 'admin/layout/main',
     extraStyles: 'order.css',
   })
 })
