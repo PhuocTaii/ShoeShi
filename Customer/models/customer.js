96bfefdf953237b9125fb3d8c9cfd70f72464a37
Van Tran <thanhvan26823@gmail.com>
2024-01-07 11:49:30 +0700

    add username when login by google

--- Customer/models/customer.js
+++ Customer/models/customer.js
@@ -10,22 +10,10 @@ const customerSchema = new mongoose.Schema({
   username: {
     type: String,
     unique: true,
-    validate: {
-      validator: function(v) {
-        return v.length >= 6 && v.length <= 20;
-      },
-      message: 'Username must be between 6 and 20 characters long'
-    },
   },
 
   password: {
     type: String,
-    validate: {
-      validator: function(v) {
-        return /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/.test(v);
-      },
-      message: 'Must be at least 8 characters. Include letters and numbers'
-    },
   },
 
   admin: {
@@ -57,12 +45,6 @@ const customerSchema = new mongoose.Schema({
 
   email: {
     type: String,
-    validate: {
-      validator: function(v) {
-        return /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/.test(v);
-      },
-      message: 'Email is invalid'
-    },
   },
 
   address: {
